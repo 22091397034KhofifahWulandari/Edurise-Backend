ab68d76385eb9d5814a97dda93cb95e054767731
22091397034_Khofifah Wulandari <124429174+22091397034KhofifahWulandari@users.noreply.github.com>
2025-06-01 20:15:30 +0700

    Add files via upload

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize ('profile_db','root','', {
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize('edurise_db', 'root', '', {
+    host: "localhost",
+    dialect: "mysql"
 });
 
 export default db;
